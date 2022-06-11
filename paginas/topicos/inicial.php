cd5d67056356e93a4f93b07abad590b2d80d0bc2
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-11 17:40:43 -0300

    replicando

---
+++ paginas/topicos/inicial.php
@@ -0,0 +1,181 @@
+<!-- <div class="container">
+    <div class="col-md position-absolute text01 container">
+        <h3>Lorem Ipsum</h3>
+        <h4> Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing
+            et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma
+            uma pindureta.</h4>
+    </div> -->
+</div>
+<div class="col-md text-center mb-0">
+    <img src="img/justica.png" alt="">
+</div>
+<div class="container text-center falecom">
+    <a href="https://wa.me/message/AQSI3RMOO6DVL1" class="decoration-none"> <button class="btn btn-whatsapp rounded-pill px-4 mt-4 fs-3 align-self-center"><i class="fab fa-whatsapp"></i> Fale com uma Advogada</button></a>
+</div>
+<div class="container text-light">
+    <div class="mt-5">
+        <div class="row nossoCompromisso">
+            <div class="col-md mt-3">
+                <h2>NOSSO COMPROMISSO</h2>
+                <h5 class="mt-4" >Cavalcanti & Leandro é um escritório especializado no Direito Previdenciário e
+                    Trabalhista, atuamos na busca pelo melhor benefício previdenciário ou assistencial através dos
+                    meios administrativo junto ao INSS ou judicial.
+                    <br>
+                    <br>
+                    Atuamos em âmbito Nacional na modalidade presencial e virtual, para proporcionar um maior conforto
+                    e praticidade para os nossos clientes.
+                    <br> <br>
+                </h5>
+            </div>
+            <div class="col-md text-center">
+                <img src="https://via.placeholder.com/400x250" alt="" class="logo">
+            </div>
+            <h5>Nosso objetivo é atender às necessidades dos nossos clientes, por isso acreditamos em um serviço com maior
+                proximidade com a advogada pela ação ou pelo acompanhamento jurídico.
+                <br>
+                Optamos por um canal direto com o nosso cliente, assim estabelecemos uma comunicação clara e objetiva,
+                com total transparência e responsabilidade.
+            </h5>
+        </div>
+    </div>
+</div>
+<div class="mb-0">
+    <div class="container p-5 pb-0 mb-4">
+
+        <h1 class="text-light">Dúvidas Frequentes</h1>
+
+        <div class="accordion text-dark mt-3" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Item #1
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>Mussum Ipsum</strong>Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram
+                        sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Item #2
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>Mussum Ipsum</strong> Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis. Todo mundo
+                        vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais interessantis.
+                        Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+                        Mé faiz elementum girarzis, nisi eros vermeio. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci
+                        ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Vehicula non. Ut sed ex eros. Vivamus sit
+                        amet nibh non tellus tristique interdum.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Item #3
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>Mussum Ipsum</strong>Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante
+                        blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus. Copo furadis é disculpa de bebadis,
+                        arcu quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
+                        Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!
+                        Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se pirulitá.
+                        A ordem dos tratores não altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non
+                        consequat odio. Per aumento de cachacis, eu reclamis. Nec orci ornare consequat. Praesent lacinia ultrices consectetur.
+                        Sed non ipsum felis.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="container text-center">
+    <a href="https://wa.me/message/AQSI3RMOO6DVL1" class="decoration-none"> <button class="btn btn-whatsapp rounded-pill px-4 mb-4 fs-2 align-self-center"><i class="fab fa-whatsapp"></i> Fale com uma Advogada</button></a>
+
+</div>
+<div class="container">
+    <div class="position-absolute">
+        <div id="carouselExampleIndicators" class="carousel slide pt-4" data-bs-ride="carousel">
+            <div class="carousel-indicators mb-0 text-center bg-dark rounded">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="10" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="11" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="12" aria-label="Slide 2"></button>
+
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/Servicos01.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos02.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos03.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos04.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos05.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos06.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos07.png" class="" alt="...">
+                    <img src="img/Servicos08.png" class="" alt="...">
+                    <img src="img/Servicos09.png" class="" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Servicos10.png" class="" alt="...">
+                    <img src="img/Servicos11.png" class="" alt="...">
+                    <img src="img/Servicos12.png" class="" alt="...">
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="col-md text-center mt-0">
+    <img src="img/justica4.png" alt="">
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <img src="img\cavalcantileandro.png" alt=" " width="300" class="mt-5 logo"> <br>
+            <a href="https://wa.me/message/AQSI3RMOO6DVL1" class="decoration-none"> <button class="btn btn-whatsapp rounded-pill px-4 mb-4 fs-5 align-self-center mt-3"><i class="fab fa-whatsapp"></i> Fale com uma Advogada</button></a>
+            <a href="mailto:contatotrabalhista@caleadvocacia.com.br" class="decoration-none"> <button class="btn btn-email rounded-pill px-4 mb-4 fs-5 mt-3 align-self-center"><i class="fa fa-envelope"></i> Fale com uma Advogada</button></a>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15804.038193970528!2d-34.8391749!3d-7.9979499!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa8a41c346a36d008!2sMayra%20Cavalcanti%20%26%20Giselle%20Leandro%20Advocacia%20%26%20Assessoria%20Jur%C3%ADdica!5e0!3m2!1spt-BR!2sbr!4v1654170674591!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="border border-4 border-light p-3"></iframe>
+        </div>
+        <div class="col border border-3 border-light rounded divcontato p-4 text-light">
+            <h3>Dúvidas? Entre em contato conosco.</h3> <br>
+            <input type="text" name="" id="" data-ls-module="charCounter" maxlength="25" placeholder="Digite seu nome*" class="d-block w-100 fs-5">
+            <input type="text" name="" id="" data-ls-module="charCounter" maxlength="11" placeholder="(xx) x.xxxx-xxxx*" class="mt-3"> <br>
+            <input type="text" name="" id="" data-ls-module="charCounter" maxlength="11" placeholder="E-mail" class="mt-3 d-block w-100">
+            <textarea type="text" name="" id="" rows="4" placeholder="Mensagem*" class="mt-3 d-block w-100 mb-0"></textarea>
+            <p class="mt-0">*Campos Obrigatórios</p>
+
+            <input type="checkbox" name="" id="" class=""><small class="fs-5"> Li e aceito o termo de Política de Pivacidade</small>
+            <a href="formulario.php" class="decoration-none"> <button class="btn btn-light rounded-pill px-4 mb-4 fs-5 mt-3 align-self-center logo">Enviar</button></a>
+        </div>
+    </div>
